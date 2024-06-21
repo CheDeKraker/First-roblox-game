90f44d230fe429c7a0353c6906fdc98ef06b2ccb
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-06-21 18:32:54 +0200

    Update coin value when player collects a coin + shows on leaderboard

--- src/ServerScriptService/Script.server.lua
+++ src/ServerScriptService/Script.server.lua
@@ -1,13 +1,29 @@
 --test!!
 
 -- Initializing services and variables
+local ServerStorage = game:GetService("ServerStorage")
 local Workspace = game:GetService("Workspace")
 local Players = game:GetService("Players")
 
 -- Getting all the coins in the world
 local coins = Workspace.World.Coins:GetChildren()
 
+--Require the modules
+local Leaderboard = require(ServerStorage.Leaderboard)
+local PlayerData = require(ServerStorage.PlayerData)
+
 local COOLDOWN = 10
+
+
+
+--Update the player's coin value
+local function updateCoinValue(player, value)
+	local x = PlayerData.updateValue(player, PlayerData.COIN_KEY_NAME, function(oldValue)
+		return oldValue + value
+	end)
+	
+	Leaderboard.setStat(player, PlayerData.COIN_KEY_NAME, x)
+end
 
 -- Defining the event handler
 local function onCoinTouched(otherPart, coin)
@@ -19,7 +35,7 @@ local function onCoinTouched(otherPart, coin)
 			coin.Transparency = 1
 			coin:SetAttribute("Enabled", false)
 			print("Player collected coin")
-			character.Humanoid.JumpHeight = character.Humanoid.JumpHeight + 20
+			updateCoinValue(player, 1)
 			task.wait(COOLDOWN)
 			coin.Transparency = 0
 			coin:SetAttribute("Enabled", true)
